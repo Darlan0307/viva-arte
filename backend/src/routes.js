80ab03c2995553951d121cb7286c877bc2fbf1db
Darlan0307 <darlanchagas2020@gmail.com>
2024-05-20 18:08:56 -0300

    feat: novos recursos

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,9 @@
 import { Router } from "express";
 import { UserController } from "./controllers/UserController.js";
-import { prisma } from "./services/prisma.js";
 
 const usercontroller = new UserController();
 
 const routes = Router();
-
-(async () => {
-  await prisma.$connect();
-})();
 
 routes.get("/", (_, res) => {
   res.json({ message: "running" });
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,24 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import routes from "./routes.js";
+import { prisma } from "./services/prisma.js";
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(async (req, res, next) => {
+  try {
+    await prisma.user.findMany();
+    next();
+  } catch (error) {
+    console.error("Erro ao aquecer conexão ao banco de dados:", error);
+    res.status(500).send("Erro interno do servidor");
+  }
+});
+
 app.use(routes);
 
 const PORT = process.env.PORT || 9001;
